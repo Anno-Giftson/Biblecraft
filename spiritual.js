bfe015d87955dfe8e97341cbaecee46214511416
Anno Giftson <anno.giftson@outlook.com>
2026-02-20 12:53:51 -0500

    Update spiritual.js

--- spiritual.js
+++ spiritual.js
@@ -1,1 +1,14 @@
+export class SpiritualState {
+  constructor() {
+    this.faith = 1;
+    this.wisdom = 1;
+  }
 
+  pray() {
+    this.faith += 1;
+  }
+
+  study() {
+    this.wisdom += 1;
+  }
+}
